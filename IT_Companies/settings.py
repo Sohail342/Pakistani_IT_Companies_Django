764abe4c1489a9a9ce0db2cfbfdb42cec212eb5f
= <sohailahmed34280@gmail.com>
2024-07-24 10:14:06 -0700

    Deployment Configrations

--- IT_Companies/settings.py
+++ IT_Companies/settings.py
@@ -15,7 +15,7 @@ SECRET_KEY = 'django-insecure-ud)=@g)lup-7sz%-z@^9fl+y2t8th+9ty@ma_sv4e1+1dm0yq@
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['127.0.0.1', '.vercel.app']
 
 
 # Application definition
@@ -29,7 +29,6 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'core',
     'registration',
-    'crispy_forms',
 ]
 
 AUTHENTICATION_BACKEBDS=[
