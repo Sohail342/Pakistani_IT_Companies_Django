094ce85860896725f6813efad24e6fe670d84542
= <sohailahmed34280@gmail.com>
2024-07-24 14:27:22 -0700

    update dependancies

--- IT_Companies/settings.py
+++ IT_Companies/settings.py
@@ -126,7 +126,6 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
-    
     ]
 
 # Default primary key field type
