9ed5922af54cd3267db62bbce10f4b12a1983d35
= <sohailahmed34280@gmail.com>
2024-07-24 10:37:26 -0700

    serving static files

--- IT_Companies/urls.py
+++ IT_Companies/urls.py
@@ -1,6 +1,8 @@
 
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 
 urlpatterns = [
@@ -9,3 +11,6 @@ urlpatterns = [
     path('software_companies/', include("core.urls")),
     
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
