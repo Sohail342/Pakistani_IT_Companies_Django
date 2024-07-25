e13fa645dc6ff836640ecd0eb277540894424c8e
= <sohailahmed34280@gmail.com>
2024-07-25 02:21:29 -0700

    remove unused dependencies

--- IT_Companies/settings.py
+++ IT_Companies/settings.py
@@ -80,10 +80,11 @@ WSGI_APPLICATION = 'IT_Companies.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-import dj_database_url
 DATABASES = {
-    
-    'default': dj_database_url.parse(env('DATABASE_URL'))
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
+    }
 }
 
 
