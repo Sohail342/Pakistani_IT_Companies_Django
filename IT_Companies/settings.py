2ee1da77940f50c627b5353d2877a4c47ae8334a
SOHAIL AHMAD <sohailahmed34280@gmail.com>
2024-08-07 06:18:08 -0700

    Update settings.py

--- IT_Companies/settings.py
+++ IT_Companies/settings.py
@@ -121,3 +121,4 @@ STATIC_ROOT = BASE_DIR / 'staticfiles'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+CSRF_TRUSTED_ORIGINS = ["https://7ap-reliable-pascal.circumeo-apps.net"]
