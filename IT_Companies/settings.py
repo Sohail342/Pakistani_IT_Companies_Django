ffd4f49ee4a596629482304c933fa8bdbca6159e
SOHAIL AHMAD <sohailahmed34280@gmail.com>
2024-08-07 05:47:16 -0700

    Update settings.py

--- IT_Companies/settings.py
+++ IT_Companies/settings.py
@@ -17,7 +17,7 @@ SECRET_KEY = os.environ.get('SECURITY_KEY', '')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['https://uj8-loyal-heisenberg.circumeo-apps.net']
 
 
 # Application definition
